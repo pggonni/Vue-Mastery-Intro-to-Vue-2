98e65f8d3571b3d3d68824f6916157521babd66b
Pg Gonni <59372630+pggonni@users.noreply.github.com>
2021-01-18 08:54:28 -0500

    Parts 1-3

---
+++ public/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './assets/vmSocks-green-onWhite.jpg',
+        inventory: 10,
+        onSale: true,
+    }
+})
